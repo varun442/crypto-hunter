af120a1bb85d7bef6056c6e632bc72973c99a059
varun <s@gmail.com>
2023-02-02 13:54:17 +0530

    .

--- src/components/Config/api.js
+++ src/components/Config/api.js
@@ -11,6 +11,6 @@ export const TrendingCoins = (currency) =>
   `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`;
 
 
-const apiKey = process.env.REACT_APP_API_KEY;
-
+// const apiKey = process.env.REACT_APP_API_KEY;
+const apiKey = "a7fd7c4feb1e439484afb36e8849b3c0"
 export const baseURL = `https://newsapi.org/v2/everything?q=cryptocurrency&apiKey=${apiKey}`
